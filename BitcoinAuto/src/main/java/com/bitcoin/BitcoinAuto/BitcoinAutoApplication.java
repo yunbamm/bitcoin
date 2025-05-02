81e2db7fe358c789216f3628744614fcbb1474d5
bangbang <fdsajklt2@naver.com>
2025-05-02 14:54:49 +0900

    Init Toy project

---
+++ BitcoinAuto/src/main/java/com/bitcoin/BitcoinAuto/BitcoinAutoApplication.java
@@ -0,0 +1,13 @@
+package com.bitcoin.BitcoinAuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitcoinAutoApplication.class, args);
+	}
+
+}
